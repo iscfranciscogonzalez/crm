eae4b2af92a7f25d4530e84f2b3e0bad271ebba3
Francisco Gonzalez <jose.gonzalez16@tectijuana.edu.mx>
2018-11-21 13:38:53 -0800

    Se termino de crear los forms

--- crm/Companies/forms.py
+++ crm/Companies/forms.py
@@ -1,1 +1,12 @@
-forms.py
+from django import forms
+from .models import Companies, Contacts
+
+class FormCompanies(forms.ModelForm):
+	class Meta:
+		model = Companies
+		fields = '__all__'
+
+class FormContacts(forms.ModelForm):
+	class Meta:
+		model = Contacts
+		fields = '__all__'
--- crm/Departments/forms.py
+++ crm/Departments/forms.py
@@ -1,1 +1,7 @@
-forms.py
+from django import forms
+from .models import Departments
+
+class FormDepartments(forms.ModelForm):
+	class Meta:
+		model = Departments
+		fields = '__all__'
